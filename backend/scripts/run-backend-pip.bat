5756821b320fac624ff899a663e5188cbec64361
Brian <54575012+brian891203@users.noreply.github.com>
2025-03-23 18:06:51 +0800

    Update run-backend-pip.bat

--- backend/scripts/run-backend-pip.bat
+++ backend/scripts/run-backend-pip.bat
@@ -7,6 +7,7 @@ if not exist .venv (
 call .venv\Scripts\activate
 
 :: install dependencies
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
 pip install -r requirements.txt
 
 :: run the app
